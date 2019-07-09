c588d43e874200936818bc6283a26ab78d901d1e
ClaudiaIldefonso <claudia@sillymatter.com>
2019-07-09 17:51:33 -0500

    modelo session bonus, high_bonus,level y info client_user

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from users.models import ClientUser, Session, StaffUser
-from game.serializers import LabelSerializer, GameSerializer
-
+from game.serializers import GameSerializer
+from django.db.models import Sum
 
 class ClientUserSerializer(serializers.ModelSerializer):
+    total_score = serializers.SerializerMethodField()
     class Meta:
         model = ClientUser
-        fields = ('club_premier_id', 'uuid')
+        fields = ('club_premier_id', 'uuid', 'total_score')
+    def get_total_score(self, client_user):
+        return Session.objects.filter(client_user_pk=client_user).aggregate(Sum('high_score'))
+
 
 
 class SessionSerializer(serializers.ModelSerializer):
