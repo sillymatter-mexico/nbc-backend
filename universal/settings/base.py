c9ca823d3d6e4115a7be7564473c4779bcff84e8
ClaudiaIldefonso <claudia@sillymatter.com>
2019-07-25 12:46:40 -0500

    cambio de email para envio de reportes

--- universal/settings/base.py
+++ universal/settings/base.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'claudiam.texcucano@gmail.com'
+EMAIL_HOST_USER = 'sillymatter.reportes@gmail.com'
 EMAIL_HOST_PASSWORD = 'claudia333ale'
 EMAIL_PORT = 587
 # Application definition
